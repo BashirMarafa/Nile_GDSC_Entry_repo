839f11286f616b8cb8b51ddd49f89a2a01432dae
BashirMarafa <bashirmarafa60@gmail.com>
2025-11-08 00:13:59 +0100

    feat: Add dark mode toggle

--- Web_Dev/Beginner/starter/script.js
+++ Web_Dev/Beginner/starter/script.js
@@ -1,1 +1,28 @@
 console.log("Welcome to your Beginner Web Dev Challenge!");
+document.addEventListener('DOMContentLoaded', () => {
+
+   
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+    const themeIcon = themeToggleButton.querySelector('i'); 
+    
+    function toggleTheme() {
+        
+        body.classList.toggle('dark-mode');
+
+        
+        if (body.classList.contains('dark-mode')) {
+           
+            themeIcon.classList.remove('fa-moon');
+            themeIcon.classList.add('fa-sun');
+        } else {
+          
+            themeIcon.classList.remove('fa-sun');
+            themeIcon.classList.add('fa-moon');
+        }
+    }
+
+    
+    themeToggleButton.addEventListener('click', toggleTheme);
+    
+});
